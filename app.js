52d14a4fce5df1d5a444c987e3d761d14f8c2c25
JVQP <jaumvit0r222@gmail.com>
2024-12-04 01:01:35 -0300

    atualizção

--- app.js
+++ app.js
@@ -46,6 +46,16 @@ app.get("/", function(req, res){
 })
 
 
+/// ROTA DA PÁGINA PRINCIPAL ////
+
+app.get('/views/pagina_principal', function(req, res) {
+    res.render('principal')
+})
+
+app.get('/views/pagina_usuario', function(req, res) {
+    res.render('usuario')
+})
+
 ////// ROTA LOGIN /////
 
 app.post("/principal", function(req, res){
@@ -75,6 +85,48 @@ db.query(query, (err, retorno) => {
 
 
 })
+
+        /*ROTA PARA CADASTRO DE USUÁRIO*/
+
+// ------------------------------------------------------ {
+
+app.post('/usuario', function(req, res){
+   
+   const nome = req.body.inputNome;
+   const sobrenome = req.body.inputSobrenome;
+   const email = req.body.inputEmail4;
+   const usuario = req.body.inputUsuario;
+   const cpf = req.body.inputCPF;
+   const rg = req.body.inputRG; 
+   const senha = req.body.inputSenha;
+   const confirmar = req.body.inputConfirmar;
+   const endereco = req.body.inputEndereco;
+   const cidade = req.body.inputCidade;
+   const estado = req.body.inputEstado;
+   const cep = req.body.inputCEP;
+   
+   const query = `INSERT INTO usuarios 
+   (nome, sobrenome, usuario, email, cpf, rg, endereco, cidade, estado, senha, senha_confirmar, cep)
+   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+   
+ console.log(nome) 
+console.log(sobrenome)
+console.log(email)
+console.log(usuario)
+console.log(rg)
+console.log(cpf)
+console.log(senha)
+console.log(confirmar)
+console.log(endereco)
+console.log(cidade)
+console.log(estado)
+console.log(cep)
+
+
+})
+
+//--------------------------------------------------------}
+
 
 ////// ROTA DO SERVIDOR ////
 
